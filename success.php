8b193646c4f01acb8e954ad8ba0864602f71e3fe
Mac <mac@MacBook-Air-Mac-2.local>
2023-03-03 11:37:15 +0200

    add

---
+++ success.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Спасибо!</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;700&display=swap" rel="stylesheet">
+    <!-- styles -->
+    <link rel="stylesheet" href="successCss/style.min.css">
+</head>
+
+<body>
+
+    <div class="page__body">
+        <div class="page__inner">
+            <div class="page__content">
+                <div class="logo">
+                    <img class="logo__link" src="succesImages/logo.svg" alt="logo">
+                </div>
+
+                <h6 class="page__title">
+                    Спасибо за обращение!
+                </h6>
+
+                <p class="page__description">
+                    В ближайшее время наш менеджер свяжется с Вами.
+                </p>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="js/main.min.js"></script>
+
+</html>
